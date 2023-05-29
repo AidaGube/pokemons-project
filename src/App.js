4bb9872607a0a33f5c78f0097bdf38b23c70064a
Aida <aidagube@gmail.com>
2023-05-30 00:24:55 +0600

    feat: I installed a react router dom and used a dynamic connection

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
-import { useState } from "react";
+import {Route, Routes } from "react-router-dom";
+import { useState, useEffect } from 'react';
+import { PokemonInfo } from "./pages/PokemonInfo";
+import {AboutPokemon} from './pages/AboutPokemon';
+import {MainPage} from './pages/MainPage';
+import { fetchPokemonS } from "./api/fetchPokemons";
 
-function App() {
-  const [theme, setTheme] = useState("dark");
+const App = () => {
+  const [theme, setTheme] = useState('dark')
   const toggleTheme = () => {
-    const newTheme = theme === "dark" ? "light" : "dark";
-    setTheme(newTheme);
-  };
+    const newTheme = theme === 'dark' ? 'light' : 'dark'
+    setTheme(newTheme)
+  }
+
+  useEffect(() => {
+    fetchPokemonS(12, 24)
+  }, [])
+
   return (
     <div className={`app ${theme}`}>
-<button onClick={toggleTheme} className="button"> Change theme </button>
-  </div>
-  ) 
+      <button onClick={toggleTheme} className="button"> Change theme </button>
+      {/* <Link to={'/about'}>
+        <button>About</button>
+      </Link> */}
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/about" element={<AboutPokemon />} />
+        <Route path="/info/:id" element={<PokemonInfo />} />
+      </Routes>
+     
+    </div>
+  );
 }
-
 export default App;
