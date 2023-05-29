74457e87763d828f473c6566b94cbc36b5b1f2f8
Aida <aidagube@gmail.com>
2023-05-29 23:51:45 +0600

    feat: created a button to change the theme on the page and wrote the logic.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [theme, setTheme] = useState("dark");
+  const toggleTheme = () => {
+    const newTheme = theme === "dark" ? "light" : "dark";
+    setTheme(newTheme);
+  };
   return (
-    <div className="App">
-    </div>
-  );
+    <div className={`app ${theme}`}>
+<button onClick={toggleTheme} className="button"> Change theme </button>
+  </div>
+  ) 
 }
 
 export default App;
