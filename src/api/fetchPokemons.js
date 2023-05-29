dde23ba75043855fb949ac0b56957c18e3e704c1
Aida <aidagube@gmail.com>
2023-05-30 00:16:03 +0600

    feat: added api

---
+++ src/api/fetchPokemons.js
@@ -0,0 +1,22 @@
+import axios from "axios"
+import { sortItem } from "../components/common/helpers"
+
+const baseUrl = 'https://pokeapi.co/api/v2/'
+
+export const fetchPokemons = async (limit, offset) => {
+    try {
+        const { data } = await axios.get(baseUrl + `pokemon/?limit=${limit}&offset=${offset}`)
+        return data
+    } catch (error) {
+        console.log('error');
+    }
+}
+export const fetchPokemonS = async (from, till, type) => {
+    let list = [];
+    for (let i = from; i < till; i++) {
+        const {data} = await axios.get(baseUrl + `pokemon/` + i)
+        list.push(data)
+    }
+    console.log(sortItem(list, 'attach'), 'sortList');
+    return sortItem(list, type)
+}
